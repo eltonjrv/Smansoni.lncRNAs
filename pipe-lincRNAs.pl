8582ae9375b5e845b7292ae474c93d537125de1a
Elton Vasconcelos <eltonjrv@gmail.com>
2017-07-25 17:15:11 -0700

    Update pipe-lincRNAs.pl

--- pipe-lincRNAs.pl
+++ pipe-lincRNAs.pl
@@ -2,14 +2,24 @@
 # Programmer: Elton Vasconcelos (Aug/2015)
 # Pipeline for identification of novel putative lincRNAs that might be present in a transcriptome assembly (e.g. trinity contigs or tuxedo TCONS)
 # This code was written by me as part of my postdoctoral research grant entitled "Identification and characterization of regulatory Long Non-coding RNAs on the Schistosoma mansoni genome through NGS strategies and systems approach", funded by Sao Paulo research Foundation (FAPESP: 14/24560-8).
-# 
+# If you use this whole pipeline or part of it, please cite this github page as well as its embedded bioinformatics tools individually
 # Five input files must be provided at the cmd-line: contigs.fasta, contigs.bed, annotated_genes.bed, repeats_library.fasta, ref-genome.fasta
-#######################################################################################################################################################################################
+###############################################################################################################################
 # Usage: nohup perl pipe-lincRNAs.pl [infile.fasta] [infile.bed] [annotated_genes.bed] [repeats_library.fasta] [ref-genome.fasta] >nohup-pipe-lincRNAs.out 2>nohup-pipe-lincRNAs.err &
-######################################################################################################################################################################################
-# Note-1: The chromosome IDs must be identical in all input files
-# Note-2: 'git clone' the "lncRNA-pipeTools" branch from this repository to your home folder at your workstation
-# Note-3: Please have bedtools, 
+################################################################################################################################
+# IMPORTANT NOTES:
+# Note 1: 'git clone' the "lncRNA-pipeTools" branch from this repository to your "home" folder at your workstation
+# Note 2: Please have bedtools (http://bedtools.readthedocs.io/en/latest/) and EMBOSS suite (http://emboss.sourceforge.net/) installed and placed at your environment variables
+# Note 3: The chromosome IDs must be identical in all input files provided
+#################################################################################################################
+# This program and its embedded tools are free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details at http://www.gnu.org/licenses/.
 
 my $base_fasta = $ARGV[0];
 $base_fasta =~ s/\.\w+$//g;

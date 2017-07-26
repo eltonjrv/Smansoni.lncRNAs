35f62f20c654af7a9ecc1b4dab9c806717f124e9
Elton Vasconcelos <eltonjrv@gmail.com>
2017-07-26 11:59:27 -0700

    Update pipe-ASlncRNAs.pl

--- pipe-ASlncRNAs.pl
+++ pipe-ASlncRNAs.pl
@@ -14,7 +14,7 @@
 # Note 3: Also take a look at the "Assembly of RNA-Seq reads" topic from "SupplementaryMethods.pdf" file (if you have no experience on transcriptome assemblies) in order to generate both "contigs.fasta" and "contigs.bed" input files.
 # Note 4: Accessories ad-hoc scripts written to support this pipeline are placed within "lncRNA-pipeTools/perl-scripts" dir.
 # Note 5: One must edit line 9 from "lncRNA-pipeTools/perl-scripts/seqtools.pl" to correctly point to his/her BioPerl full PATH, as well as line 31 from "lncRNA-pipeTools/perl-scripts/seqs1.pl", replacing the seqtools.pl right location.
-# Note 6: The chromosome IDs must be identical in the two bed files as well as in the ref-genome.fasta provided on the cmd-line.
+# Note 6: The chromosome IDs must be identical on the two bed input files as well as on the ref-genome.fasta provided in the command line
 # Note 7: All the FASTA and BED output files generated during the execution of this pipeline are named intuitively, indicating the filtering steps that have been performed.
 #################################################################################################################
 # This program and its embedded tools are free software: you can redistribute it and/or modify
